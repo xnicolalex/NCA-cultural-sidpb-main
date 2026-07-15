684edce24236fb216dfa83ece52a4b4c281287f1
Rochel Andrey <rochelinho@gmail.com>
2026-07-15 10:49:18 -0300

    next config changes

--- Website-fotografo-fork-main/next.config.mjs
+++ Website-fotografo-fork-main/next.config.mjs
@@ -1,25 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Permite conexões externas (WebSockets/HMR) no modo Dev dentro do Docker
+  basePath: '/cultura',
+  trailingSlash: true,
   allowedDevOrigins: [
     'localhost:3001',
     'localhost:3000',
-    '192.168.200.186' 
+    '192.168.200.186'
   ],
-  
   typescript: {
-    ignoreBuildErrors: true, // ajustar para false após fase inicial de projeto
+    ignoreBuildErrors: true,
   },
-  
   images: {
     unoptimized: true,
   },
 }
-
 export default nextConfig
-
-// Redirecionamentos
-// Variáveis de ambiente públicas
-// Domínios externos de imagem
-// Headers customizados
-// Modo de saída
--- sidpb-frontend-dev/next.config.mjs
+++ sidpb-frontend-dev/next.config.mjs
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/sidpb",
+  trailingSlash: true,
   typescript: {
-    // Ignora o erro do react-hook-form
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Ignora erros de variáveis não usadas
-    ignoreDuringBuilds: true,
-  },
 };
-
 export default nextConfig;
